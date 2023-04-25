8abf8d7535ddc8c88279964de5ae05c46da3ea1a
derwaro <chrdina@gmail.com>
2023-04-24 21:57:11 -0600

    rewrite

--- txtbg.py
+++ txtbg.py
@@ -18,90 +18,71 @@ board[12] = "R5"
 board[13] = "B5"
 
 
+def throw_dice(first_throw=False):
+    a, b = random.randint(1, 6), random.randint(1, 6)
+    if first_throw == True:
+        return a, b
+    else:
+        if a > b:
+            return str(a) + str(b)
+        elif b > a:
+            return str(b) + str(a)
+        else:  # a double throw returns the value 4 times
+            return str(a) * 4
+
+
 def choose_starting_player():
     red, black = 1, 1
     while red == black:
-        red, black = random.randint(1, 6), random.randint(1, 6)
+        red, black = throw_dice(first_throw=True)
     if red > black:
         return "R", red, black
     elif black > red:
         return "B", red, black
 
 
-def order_dice_throw(i, j):
-    if i > j:
-        return str(i) + str(j)
-    elif j > i:
-        return str(j) + str(i)
-    else:  # a double throw returns the value 4 times
-        return str(i) * 4
-
-
 # create variable for current turn and set it based on a dice roll between 2d6. Get the result of the dice throw and store it in variables to get the first dice result
 current_player, red, black = choose_starting_player()
 
 # order the initial throw (values in red and black):
-current_dice = order_dice_throw(red, black)
-
-
-def print_board():  # this is it!
-    for i in range(13, 19):
-        print(f"{i:<3}|", end="")
-    print("<||||>", end="")
-    for i in range(19, 25):
-        print(f"{i:<3}|", end="")
-    print("")
-    for i in range(13, 19):
-        print(f"{board[i]:<3}|", end="")
-    print("<||||>", end="")
-    for i in range(19, 25):
-        print(f"{board[i]:<3}|", end="")
-    print("")
+if red > black:
+    current_dice = str(red) + str(black)
+elif black > red:
+    current_dice = str(black) + str(red)
+else:
+    current_player, red, black = choose_starting_player()
 
 
 def print_board():
-    for key, value in board.items():
-        if type(key) == int:
-            if key >= 13 and key <= 18:
-                print(str(key) + " | ")
-                print("---")
-                if value != "":
-                    print(value + " | ", end="")
-                else:
-                    print("<>", end="")
-            if key >= 19 and key <= 24:
-                print(str(key) + " | ")
-                print("---")
-                if value != "":
-                    print(value + " | ", end="")
-                else:
-                    print("<>", end="")
-            if key == 25:
-                print("HOME")
-                print("---")
-                print(value)
-    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    print("~~~~~~~~~~~~~ BAR: " + board["bar"] + "~~~~~~~~")
-    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    for key, value in board.items():
-        if type(key) == int:
-            if key <= 12 and key >= 7:
-                print(str(key) + " | ")
-                print("---")
-                if value != "":
-                    print(value + " | ", end="")
-                else:
-                    print("<>", end="")
-            if key <= 6 and key <= 1:
-                print(str(key) + " | ")
-                print("---")
-                if value != "":
-                    print(value + " | ", end="")
-                else:
-                    print("<>", end="")
-            if key == 0:
-                print("HOME")
-                print("---")
-                print(value)
+    for i in range(13, 19):
+        print(f"{i:<3}|", end="")
+    print("<||||>", end="")
+    for i in range(19, 25):
+        print(f"{i:<3}|", end="")
+    print("")
+    for i in range(13, 19):
+        print(f"{board[i]:<3}|", end="")
+    print("<||||>", end="")
+    for i in range(19, 25):
+        print(f"{board[i]:<3}|", end="")
+    print("")
+    print("~" * 54)
+    print("~" * 25 + "BAR" + "~" * 26)
+    bar_value = (
+        str(board["bar"]).center(54, "~")
+        if board["bar"] != ""
+        else "XXX".center(54, "~")
+    )
+    print(bar_value)
+    print("~" * 54)
+    for i in range(12, 6, -1):
+        print(f"{i:<3}|", end="")
+    print("<||||>", end="")
+    for i in range(6, 0, -1):
+        print(f"{i:<3}|", end="")
+    print("")
+    for i in range(12, 6, -1):
+        print(f"{board[i]:<3}|", end="")
+    print("<||||>", end="")
+    for i in range(6, 0, -1):
+        print(f"{board[i]:<3}|", end="")
